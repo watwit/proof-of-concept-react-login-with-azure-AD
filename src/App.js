cb560c02e2ad4f8ad44a68d02c928d8c73638528
watwit <59312631+watwit@users.noreply.github.com>
2023-02-20 14:44:05 +0700

    success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { config } from './Config';
+import { PublicClientApplication } from '@azure/msal-browser';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      error:null,
+      isAuthenticated:false,
+      user:{}
+    };
+
+    this.login = this.login.bind(this);
+
+    this.publicClientApplication = new PublicClientApplication({
+      auth:{
+        clientId:config.appId,
+        redirectUri:config.redirectUrl,
+        authority:config.authority
+      },
+      cache:{
+        cacheLocation:"sessionsStorage",
+        storeAuthStateInCookie:true
+      }
+    })
+  }; 
+
+  async login(){
+    try{
+      await this.publicClientApplication.loginPopup({
+        scopes:config.scopes,
+        prompt:"select_account"
+      });
+      this.setState({isAuthenticated:true})
+
+    }catch(err){
+      this.setState({isAuthenticated:false,user:{},error:err})
+
+    }
+  }
+  logout(){
+    this.publicClientApplication.logout();
+  }
+  render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>test</p>
+            {this.state.isAuthenticated ? <p>login success</p> : <button onClick={()=> this.login()}>Login</button>}
+          </header>
+        </div>
+      );
+  }
 }
-
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,6 @@
+export const config = {
+    appId:'<AppId>',
+    redirectUrl:'http://localhost:3000',
+    scopes:['user.read'],
+    authority:'https://login.microsoftonline.com/<Tenant ID>'
+  };
